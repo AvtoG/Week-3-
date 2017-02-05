c53bf7e4ee1542375640ae93e72f7cafeaeb5417
AvtoG <agrigolia@cca.edu>
2017-02-05 15:03:06 -0800

    Create sketch-5.js

---
+++ sketch-5.js
@@ -0,0 +1,15 @@
+function setup() { 
+  createCanvas(400, 400);
+}
+
+function draw() { 
+	y=200
+	for(var x=50; x<=width; x+=100){
+		for(var w=20; w<=80; w+=10){
+			noFill()
+			rectMode(CENTER)
+			rect(x,y,w,w)
+			
+	}
+}
+}
